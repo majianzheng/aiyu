0c45a78721676ba3ba2bbd0acaabb4d09cc51adf
majianzheng <282295811@qq.com>
2024-09-04 13:15:28 +0800

    hotfix: client-cli同步启停

--- tools/src/main/java/io/github/majianzheng/jarboot/tools/client/command/ServiceCommand.java
+++ tools/src/main/java/io/github/majianzheng/jarboot/tools/client/command/ServiceCommand.java
@@ -1,10 +1,15 @@
 package io.github.majianzheng.jarboot.tools.client.command;
 
 import io.github.majianzheng.jarboot.api.cmd.annotation.*;
+import io.github.majianzheng.jarboot.api.constant.TaskLifecycle;
+import io.github.majianzheng.jarboot.api.event.JarbootEvent;
+import io.github.majianzheng.jarboot.api.event.Subscriber;
+import io.github.majianzheng.jarboot.api.event.TaskLifecycleEvent;
 import io.github.majianzheng.jarboot.api.pojo.ServiceInstance;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * list command
@@ -16,7 +21,7 @@ import java.util.List;
         "  service -h 127.0.0.1:9899 start demo-server\n" +
         "  service stop demo-service\n" +
         "  service restart demo-service\n")
-public class ServiceCommand extends AbstractClientCommand {
+public class ServiceCommand extends AbstractClientCommand implements Subscriber<TaskLifecycleEvent> {
     private static final String ACTION_START = "start";
     private static final String ACTION_STOP = "stop";
     private static final String ACTION_RESTART = "restart";
@@ -24,6 +29,7 @@ public class ServiceCommand extends AbstractClientCommand {
     private String action;
     private String serviceName;
     private String host;
+    private CountDownLatch latch;
 
     @Option(shortName = "h", longName = "host")
     @Description("Cluster host")
@@ -49,22 +55,69 @@ public class ServiceCommand extends AbstractClientCommand {
         instance.setHost(host);
         instance.setName(serviceName);
         service.add(instance);
-        switch (action) {
-            case ACTION_START:
-                client.startService(service);
-                break;
-            case ACTION_STOP:
-                client.stopService(service);
-                break;
-            case ACTION_RESTART:
-                client.restartService(service);
-                break;
-            default:
-                printHelp();
+        client.registerSubscriber(this);
+        final String tips = "Service " + serviceName + " " + action;
+        try {
+            latch = new CountDownLatch(1);
+            switch (action) {
+                case ACTION_START:
+                    client.startService(service);
+                    break;
+                case ACTION_STOP:
+                    client.stopService(service);
+                    break;
+                case ACTION_RESTART:
+                    client.restartService(service);
+                    break;
+                default:
+                    printHelp();
+                    return;
+            }
+            final long timeout = 30;
+            if (latch.await(timeout, java.util.concurrent.TimeUnit.SECONDS)) {
+                println(tips + " success.");
+            } else {
+                println(tips + " timeout.");
+            }
+        } catch (InterruptedException e) {
+            println(tips + " interrupted.");
+            Thread.currentThread().interrupt();
+        } finally {
+            client.deregisterSubscriber(this);
         }
     }
 
     @Override
     public void cancel() {  // default implementation ignored
+    }
+
+    @Override
+    public void onEvent(TaskLifecycleEvent event) {
+        synchronized (this) {
+            switch (action) {
+                case ACTION_START:
+                case ACTION_RESTART:
+                    if (event.getLifecycle() == TaskLifecycle.FINISHED ||
+                            event.getLifecycle() == TaskLifecycle.AFTER_STARTED ||
+                            event.getLifecycle() == TaskLifecycle.START_FAILED ||
+                            event.getLifecycle() == TaskLifecycle.SCHEDULING) {
+                        latch.countDown();
+                    }
+                    break;
+                case ACTION_STOP:
+                    if (event.getLifecycle() == TaskLifecycle.AFTER_STOPPED ||
+                            event.getLifecycle() == TaskLifecycle.STOP_FAILED) {
+                        latch.countDown();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public Class<? extends JarbootEvent> subscribeType() {
+        return TaskLifecycleEvent.class;
     }
 }
